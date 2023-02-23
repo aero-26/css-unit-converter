4c7195793324c28f916a9f9047cfd9b71398c871
Shubhankar Banerjee <shubhankarbanerjee16@gmail.com>
2023-02-23 18:50:32 +0530

    Restricted invalid input.

---
+++ script.js
@@ -0,0 +1,16 @@
+let body = document.querySelector("body");
+
+body.addEventListener("keydown", (event)=>{
+    let key = Number(event.key);
+    let keycode = event.keyCode;
+    console.log(keycode);
+    let f12 = Boolean(keycode === 123);
+    let backspace = Boolean(keycode === 8); 
+    let period = Boolean(keycode === 190);
+    let zero = Boolean(keycode === 48);
+   if (!key && !backspace && !period && !zero && !f12) {
+       event.preventDefault();
+   }; 
+})
+
+
