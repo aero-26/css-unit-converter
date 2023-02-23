8183d19dfd946ca180bbf7effba438d9eaef44ef
Shubhankar Banerjee <shubhankarbanerjee16@gmail.com>
2023-02-23 18:54:58 +0530

    Edited JS.

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ body.addEventListener("keydown", (event)=>{
     let keycode = event.keyCode;
     console.log(keycode);
     let f12 = Boolean(keycode === 123);
+    let f11 = Boolean(keycode === 122);
     let backspace = Boolean(keycode === 8); 
     let period = Boolean(keycode === 190);
     let zero = Boolean(keycode === 48);
-   if (!key && !backspace && !period && !zero && !f12) {
+   if (!key && !backspace && !period && !zero && !f12 && !f11) {
        event.preventDefault();
    }; 
 })
